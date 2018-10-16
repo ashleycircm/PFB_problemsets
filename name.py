27cca8e856cd59770b89aeb3b8281b8f967c6711
ach <ach@pfb13.cshl.edu>
2018-10-16 12:00:48 -0400

    name.py

---
+++ name.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import sys
+name = "Ashley"
+print (name)
+colour = "purple"
+print (colour)
+activity = "skiing"
+print (activity)
+animal = "sheep"
+print (animal)
+
+name1 = sys.argv[1] # get first command line parameter
+colour2 = sys.argv[2] # get second command line parameter
+activity3 = sys.argv[3]
+animal4 = sys.argv[4]
+
+print (name1, 'and', colour2, 'and', activity3, 'and', animal4)
+
+ 
+
+
+
+
